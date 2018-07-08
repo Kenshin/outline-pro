dbd23b3e749eb65e3d5352e34550bbd80746ceb0
Kenshin <fxblog@gmail.com>
2018-07-08 15:30:05 +0800

    Add italic convert.

---
+++ src/plugin/italic.js
@@ -0,0 +1,27 @@
+console.log( "=== outline plugin: italic load ===" )
+
+const target = '_';
+
+function toItalic( str ) {
+    console.log( "italic str before is", str )
+    const arr = str.match( /_[\S ][^_]+_/ig );
+    if ( arr && arr.length > 0 ) {
+        const repl = arr.map( item => {
+            return item.replace( /^_/, '<outline class="italic">' ).replace( /_$/, '</outline>' );
+        });
+        arr.forEach( ( item, idx ) => {
+            str = str.replace( arr[idx], repl[idx] );
+        });
+    }
+    console.log( "italic str after is", str, arr )
+    return str;
+}
+
+function fromItalic( str ) {
+    str = str.replace( /<outline class="italic">/ig, '_' ).replace( /<\/outline>/ig, '_' );
+}
+
+export {
+    toItalic,
+    fromItalic,
+}
--- src/plugin/main.js
+++ src/plugin/main.js
@@ -1,17 +1,20 @@
 console.log( "=== outline plugin: main load ===" )
 
-import * as code from './code';
-import * as bold from './bold';
+import * as code   from './code';
+import * as bold   from './bold';
+import * as italic from './italic';
 
 function convert( str ) {
     str = code.toCode( str );
     str = bold.toBold( str );
+    str = italic.toItalic( str );
     return str;
 }
 
 function recovery( str ) {
     str = code.fromCode( str );
     str = bold.fromBold( str );
+    str = italic.fromItalic( str );
     return str;
 }
 
