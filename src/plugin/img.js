cc4fad6bc66380bf604c94c727c8c9c01a2a29ca
Kenshin <fxblog@gmail.com>
2018-07-09 12:41:42 +0800

    Optimize img convert.

--- src/plugin/img.js
+++ src/plugin/img.js
@@ -1,6 +1,6 @@
 console.log( "=== outline plugin: img load ===" )
 
-const target = 'http\S+.(png|gif|jpg)|\!\[\]\(http\S+.(png|gif|jpg)\)';
+const target = 'http\S+.(png|gif|jpg)|\!\[[\S ]+\]\(http\S+.(png|gif|jpg)\)';
 
 function init() {
     $( '#paper' ).on( 'mouseover', '.outline-img', event => {
@@ -16,10 +16,20 @@ init();
 
 function toImg( str ) {
     console.log( "img str before is", str )
-    const arr = str.match( /http\S+.(png|gif|jpg)/ig );
+    const arr = str.match( /\!\[[\S ]*\]\(http\S+.(png|gif|jpg)\)/ig ),
+          title = str => {
+            str = str.match( /!\[[\S ]*\]/ );
+            str = str[0].replace( '![', '' ).replace( ']', '' );
+            return str;
+          },
+          href = str => {
+            str = str.match( /http\S+.(png|gif|jpg)/ );
+            str = str[0];
+            return str;
+          };
     if ( arr && arr.length > 0 ) {
         const repl = arr.map( item => {
-            return `<a class="outline-img" href=${item} target="_blank"><i class="fas fa-image"></i> 已插入图片</a>`
+            return `<a class="outline-img" href=${href(item)} target="_blank"><i class="fas fa-image"></i> ${title(item)}</a>`
         });
         arr.forEach( ( item, idx ) => {
             str = str.replace( arr[idx], repl[idx] );
