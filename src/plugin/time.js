05371ec46e0b7c3c07a5c28770980b371fe66dc0
Kenshin <fxblog@gmail.com>
2018-07-09 15:13:56 +0800

    Remove old source.

--- src/plugin/time.js
+++ src/plugin/time.js
@@ -4,18 +4,6 @@ import timeago from 'timeago';
 
 const target = '[2018-07-10 15:00] [2018-07-10]';
 
-function init() {
-    $( '#paper' ).on( 'mouseover', '.outline-time', event => {
-        $( event.target ).append( `<time src=${event.target.href}></time>` );
-        $( event.target ).parent().parent().addClass( 'outline-time-deep' );
-    });
-    $( '#paper' ).on( 'mouseout', '.outline-time', event => {
-        $( event.target ).find( 'time' ).remove();
-        $( event.target ).parent().parent().removeClass( 'outline-time-deep' );
-    });
-}
-//init();
-
 function toTime( str ) {
     console.log( "time str before is", str )
     const arr = str.match( /\[\d{4}-\d{2}-\d{2}( \d{2}:\d{2})?\]/ig ),
