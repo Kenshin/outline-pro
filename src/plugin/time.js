89f55ce087a71ecd918de7cea8de5bd83801fbdb
Kenshin <fxblog@gmail.com>
2018-07-10 10:04:38 +0800

    Add todo plugin and optimize time|date source.

--- src/plugin/time.js
+++ src/plugin/time.js
@@ -1,41 +1,8 @@
 console.log( "=== outline plugin: time load ===" )
 
-import timeago from 'timeago';
-
-import storage from 'storage';
+import * as todo from './todo';
 
 const target = '[2018-07-10 15:00] [2018-07-10]';
-
-/**
- * Get now time
- * 
- * @return {string} return now, e.g. 2017年04月03日 11:43:53
- */
-function convert( day ) {
-    const date   = new Date( day ),
-          format = value => value = value < 10 ? "0" + value : value;
-    return date.getFullYear() + "-" + format( date.getUTCMonth() + 1 ) + "-" + format( date.getUTCDate() ) + " " + format( date.getHours() ) + ":" + format( date.getMinutes() ) + ":" + format( date.getSeconds() );
-}
-
-/**
- * Create <outline time="2018-07-09 15:00:00" class="time ago"><i class="fas fa-calendar-alt"></i> 18 小时前</outline>
- * 
- * @param  {array} arr
- * @param  {func} ago
- * @return {array}
- */
-function format( arr, ago ) {
-    return arr.map( item => {
-        let date     = item;
-        date         = date.replace( /\[|\]/ig, '' ).trim();
-        date         = ago( date );
-        const fmt    = timeago().format( date , 'zh_CN' ),
-              agoCls = fmt.includes( '前' ) ? 'ago' : '',
-              time   = convert( date );
-        storage.todo.push( time );
-        return `<outline time="${time}" class="time ${agoCls}"><i class="fas fa-calendar-alt"></i> ${fmt}</outline>`;
-    });
-}
 
 function toTime( str ) {
     console.log( "time str before is", str )
@@ -44,7 +11,7 @@ function toTime( str ) {
             return new Date( str + ':00' );
           };
     if ( arr && arr.length > 0 ) {
-        const repl = format( arr, ago );
+        const repl = todo.timeTmpl( arr, ago );
         arr.forEach( ( item, idx ) => {
             str = str.replace( arr[idx], repl[idx] );
         });
