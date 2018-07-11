0109d6f38efecc5cc9980fa140e56265b463b32f
Kenshin <fxblog@gmail.com>
2018-07-11 14:50:29 +0800

    Add [today] to convert.

--- src/plugin/date.js
+++ src/plugin/date.js
@@ -2,7 +2,7 @@ console.log( "=== outline plugin: date load ===" )
 
 import * as todo from './todo';
 
-const target = '[now|tomorrow|week]',
+const target = '[now|tomorrow|week|today]',
       ago = str => {
         let hour = 1000 * 60 * 60,
             now  = Date.now(),
@@ -10,6 +10,11 @@ const target = '[now|tomorrow|week]',
         switch ( str ) {
             case 'now':
                 date = now;
+                break;
+            case 'today':
+                date = new Date();
+                const format = value => value = value < 10 ? "0" + value : value;
+                date = date.getFullYear() + "-" + format( date.getUTCMonth() + 1 ) + "-" + format( date.getUTCDate() ) + " 23:59";
                 break;
             case 'tomorrow':
                 date = now + ( hour * 24 );
@@ -23,7 +28,7 @@ const target = '[now|tomorrow|week]',
 
 function toDate( str ) {
     //console.log( "date str before is", str )
-    const arr = str.match( /\[(now|tomorrow|week)\]/ig );
+    const arr = str.match( /\[(now|tomorrow|week|today)\]/ig );
     if ( arr && arr.length > 0 ) {
         const repl = todo.timeTmpl( arr, ago );
         arr.forEach( ( item, idx ) => {
@@ -36,13 +41,13 @@ function toDate( str ) {
 
 function replace( $target, str ) {
     if ( str == undefined ) return str;
-    const arr = str.match( /\[(now|tomorrow|week)\]/ig );
+    const arr = str.match( /\[(now|tomorrow|week|today)\]/ig );
     if ( arr && arr.length > 0 ) {
         let time = arr[0].replace( /\[|\]/ig, '' ).trim();
         time     = ago( time );
         time     = todo.fmtDate( time );
         time     = time.replace( /:\d{2}$/ig, '' );
-        return str.replace( /\[(now|tomorrow|week)\]/ig, `[${time}]` );
+        return str.replace( /\[(now|tomorrow|week|today)\]/ig, `[${time}]` );
     }
     return str;
 }
